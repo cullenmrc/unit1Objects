929ddb4dce4a37584aa9e0a37a81d68d5b99a73d
Geoff Schmit <gcschmit@gmail.com>
2016-08-22 14:12:44 -0500

    Notes
    started chapter 1; left off just before identifiers

---
+++ Notes/HelloPrinter.java
@@ -0,0 +1,44 @@
+
+/**
+ * Every Java program contains one or more classes (e.g., HelloPrinter)
+ * 
+ * In general, every source file (HelloPrinter.java) contains one
+ *  class (HelloPrinter).
+ * 
+ * The source file name must match class name.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HelloPrinter
+{
+    /* A class contains methods.
+     * 
+     * Every Java application contains a class with a main method.
+     * 
+     * The main method is executed when the application starts.
+     */
+    public static void main( String[] args )
+    {
+        /* A method contains statements.
+         * 
+         * Statements may invoke other methods (e.g., println).
+         * 
+         * In general, methods are invoked on objects (e.g., System.out).
+         * 
+         * When invoking a method, arguments are passed in parentheses
+         *  (e.g., "Hello World!").
+         */
+        System.out.println( "Hello, World!" );
+        
+        // this is a single-line comment
+        
+        
+    }
+}
+
+
+
+
+
+
