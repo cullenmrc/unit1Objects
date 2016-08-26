0e108aa5f650055ce2a936b3fbc88c143bd5b8b7
Geoff Schmit <gcschmit@gmail.com>
2016-08-26 14:13:18 -0500

    More chapter 2 notes
    through slide 11

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -24,12 +24,60 @@ public class TurtleWorld
          *  World and Turtle are classes
          */
         World turtleWorld = new World();
+        
+        /*
+         * We use the new operator to construct (create) an object.
+         *  The class of the object is specified immediately after the
+         *      new operator (e.g. Turtle).
+         *  If we have to pass addition information to create the object,
+         *      arguments are specified in parentheses after the class
+         *      (e.g., turtleWorld).
+         *  A reference to the newly created object is returned by the new
+         *      operator.
+         */
         Turtle turtle = new Turtle(turtleWorld);
+        
+        /* 
+         * When invoking methods, we use the dot operator to invoke method on
+         *      an object.
+         *   Some methods take no arguments, but we still have parentheses
+         *      (e.g., penDown)
+         *   Some methods take one or more arguments (e.g., forward)
+         *   
+         *   Mutator methods modify the state (i.e. properties) of the object.
+         *      setPenColor, penDown, and forward are all mutator methods
+         */
         turtle.setPenColor(Color.RED);
         turtle.penDown();
         turtle.forward(50);
+        
+        /*
+         * Accessor methods return a property of the object.
+         *  The state of the object does not change.
+         *  
+         *  getPenWidth is an accessor method.
+         */
+        int penWidth = turtle.getPenWidth();
+        
+        /*
+         * Java has several primitive data types:
+         *      boolean holds a value of true or false
+         *      int holds a integer number
+         *      double holds a real number (floating point)
+         *      char holds a single character
+         */
+        // here is a boolean
+        boolean isSummer = true;
+        
+        // here is a double
+        double taxRate = 0.075;
+        
+        // here is a char
+        char letter = 'c';
     }
 }
+
+
 
 
 
